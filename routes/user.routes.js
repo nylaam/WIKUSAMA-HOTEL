e3dafd33f022e6d240b461502ae728ab26f6ac3f
Nayla Nur Amalia <nayla_amalia_30rpl@student.smktelkom-mlg.sch.id>
2023-02-15 12:51:02 +0700

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,13 +5,14 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 const userController = require(`../controller/user_controller`)
+const router = new express.Router()
 const auth = require(`../auth/auth`)
 
 app.post("/login", userController.login)
 app.get("/getAllUser", auth.authVerify,userController.getAllUser)
+app.post("/find", userController.findUser)
 app.post("/add", auth.authVerify,userController.addUser)
-app.post("/find", auth.authVerify,userController.findUser)
-app.put("/:id", auth.authVerify,userController.updateUser)
-app.delete("/:id", auth.authVerify,userController.deleteUser)
+app.put("/updateUser/:id", auth.authVerify,userController.updateUser)
+app.delete("/deleteUser/:id", auth.authVerify,userController.deleteUser)
 
 module.exports = app
